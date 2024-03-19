e1980af0a210098c3b0d1605070f420b4a03e452
quintadi <grafistero@hotmail.com>
2024-03-19 03:16:08 +0000

    3 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "hybrid",
-  // adapter: vercel({
-  //   imageService: false
-  // }),
-  integrations: [
-    tailwind({
-      // Example: Allow writing nested CSS declarations
-      // alongside Tailwind's syntax
-      nesting: true,
-    }),
-  ],
+  output: "server",
+  adapter: vercel({
+    imageService: false
+  }),
+  integrations: [tailwind({
+    // Example: Allow writing nested CSS declarations
+    // alongside Tailwind's syntax
+    nesting: true
+  }), svelte()]
 });
